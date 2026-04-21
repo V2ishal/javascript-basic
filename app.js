834f9f619c87cc392d40535483df405b88fbcfd2
V2ishal <vishalmhaske928@gmail.com>
2026-04-21 11:23:17 +0530

    update

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
 // floyed s triangle
+let n=5;
+let count=1;
 
+for(let i=1; i<=n; i++){
+let add="";
+    for(let j=1; j<=i; j++){
+        add+=count;
+        count++;
+    }
+    console.log(add);
+}
 

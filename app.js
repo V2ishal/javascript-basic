a3d85239c2a93de75258b57a4fa647acdfa1025b
V2ishal <vishalmhaske928@gmail.com>
2026-05-09 14:33:34 +0530

    update

--- app.js
+++ app.js
@@ -1,39 +1,4 @@
 
-//⚡ Next (Q7):
-// total marks find karo
-//Expected:
-//80 + 90 = 170
-//Hint 😏:
-//student.marks.math + student.marks.science
-// let student={
-//     name:"vishal",
-//     marks:{
-//         math:80,
-//         science:90
-//     }
-// };
-// console.log(student.marks.math+student.marks.science);
-
-//object ke andar function add karo:
-//Expected:
-//greet() → "Hello Vishal"
-
-//Hint 
-//function ko object ke andar likhte hai
-//this.name use karna
-
-let student={
-    name:"vishal",
-    marks:{
-        math:80,
-        science:90
-    },
-    greet(){
-        console.log("hello vishal");
-    }
-};
-console.log(student.greet());
-
 
 
 

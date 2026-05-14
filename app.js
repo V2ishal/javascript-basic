3ae54cc1f6a8bb650b4d460945d1d6b68efce323
V2ishal <vishalmhaske928@gmail.com>
2026-05-14 11:15:48 +0530

    update

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
+//Q 1 string reverse
+let str=[1,2,3,4,5];
 
+for (let i = 0; i<str.length/2; i++){
+    let temp=str[i];
+    str[i]=str[str.length-1-i];
+    str[str.lenght-1-i]=temp; 
+}
+console.log(str);

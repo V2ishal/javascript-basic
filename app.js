acbef5c2ccda0c4c352f0ed5be346e68b18cb59e
V2ishal <vishalmhaske928@gmail.com>
2026-05-15 12:57:20 +0530

    update

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-// Q2 palindrome
+// Q2 string palindrome or not?
+
+let str="madam";
+let ispalindrome="true";
+
+for(let i=0; i<str.length; i++){
+
+    if(str[i]!==str[str.length-1-i]){
+        ispalindrome="false";
+        break;
+    }
+}
+console.log(ispalindrome);

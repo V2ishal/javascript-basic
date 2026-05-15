2d59cb6d00d1708964ba1a4143a777e9d2b50f27
V2ishal <vishalmhaske928@gmail.com>
2026-05-15 10:33:21 +0530

    update

--- app.js
+++ app.js
@@ -1,14 +1,1 @@
-//Q2 palindrome
-let str=["madam"];
 
-let palindrome="";
-for(let i=0; i<str.length; i++){
-
-    if(str[i]===str[str.lenght-1-i]){
-      palindrome="true";
-    }
-    else{
-        palindrome="false";
-    }
-}
-console.log(palindrome);

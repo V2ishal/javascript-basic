c24ec56e050763ec9aad23c6b30a21813227bea0
V2ishal <vishalmhaske928@gmail.com>
2026-05-19 13:02:12 +0530

    update

--- app.js
+++ app.js
@@ -1,13 +1,10 @@
-//2 string is palindrome or not
+// string is plaindrome or not
 let str="madam";
-let palindrome="yes";
-
-for(let i=0; i<str.length/2; i++){
-
-    if(str[i] !== str[str.length-1-i]){
-        palindrome="false";
-        break;
-}
+let palindrome="true";
+for(let i=0; i<str.length; i++){
+   if(str[i]!==str[str.length-1-i]){
+    palindrome="false";
+   }
 }
 console.log(palindrome);
 

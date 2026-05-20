f54eeb395219381d534fee9f670ddad7596f7f80
V2ishal <vishalmhaske928@gmail.com>
2026-05-20 12:18:32 +0530

    update

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 //Q1 string reverse
-let str="mhaske";
-let newstr="";
-for(let i=str.length-1; i>=0; i--){
-    newstr+=str[i];
+let str="vishal";
+let newstring="";
+
+for(let i=str.length-1; i>=0; i++){
+   newstring=str[i];
 }
-console.log(newstr);
+console.log(newstring);

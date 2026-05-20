7d11fc76513d789cc30b2d3061225c3052408247
V2ishal <vishalmhaske928@gmail.com>
2026-05-20 13:41:57 +0530

    update

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 // string is plaindrome or not
-let str="madam";
+let str="vishal";
 let palindrome="true";
-for(let i=0; i<str.length; i++){
+
+for(let i=0; i<str.length/2; i++){
+
    if(str[i]!==str[str.length-1-i]){
-    palindrome="false";
+      palindrome="false";
+      break;
    }
 }
 console.log(palindrome);
+
+
 

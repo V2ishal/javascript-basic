f18e472214e17cd585fb40c795f32381356febf7
V2ishal <vishalmhaske928@gmail.com>
2026-05-21 13:56:47 +0530

    update

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-//q5 duplicate remove
+//q5 duplicate remove using include method
+let str="proggrram";
+let result="";
+
+for(let i=0; i<str.length; i++){
+   let ch=str[i];
+
+   if(!result.includes(ch)){
+      result+=ch;
+   }
+}
+console.log(result);
+
+

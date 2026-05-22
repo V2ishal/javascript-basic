7e9ca557507e14f6ac5b5f9b8f7f6e4acf00384b
V2ishal <vishalmhaske928@gmail.com>
2026-05-22 20:46:23 +0530

    update

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 // vowels count using normal for loopes
-let str="vishal";
+let str="rahul";
 let count=0;
 
 for(let i=0; i<str.length; i++){
    str[i];
-
-   if(str[i]=="a"|| str[i]=="e"||str[i]=="i"||str[i]=="o"||str[i]=="u"){
-      count++;
+   if(str[i]==="a"||str[i]==="e"||str[i]==="i"||str[i]==="o"||str[i]==="u"){
+      count++
    }
 }
 console.log(count);

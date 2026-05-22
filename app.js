55a18b586a4b11846099be8a0eb9353e6e8d8ec5
V2ishal <vishalmhaske928@gmail.com>
2026-05-22 13:47:07 +0530

    update

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 // string is plaindrome or not
-let str="vishal";
+let str="madam";
+
 let palindrome="true";
 
 for(let i=0; i<str.length/2; i++){
-
    if(str[i]!==str[str.length-1-i]){
       palindrome="false";
-      break;
    }
 }
 console.log(palindrome);

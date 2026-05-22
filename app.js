51696fed55fcdf15da6c9b3ad7369d9d5f7f5b8c
V2ishal <vishalmhaske928@gmail.com>
2026-05-22 14:20:22 +0530

    update

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 //q3 count vowels in string using for of loopes
-
 let str="vishal";
 let count=0;
 
-for( ch of str){
+for(ch of str){
 
-   if(ch==="a"|| ch==="e"||ch==="i"|| ch==="o"|| ch==="u"){
-      count++;
+   if(ch==="a"||ch==="e"||ch==="i"||ch==="o"||ch==="u"){
+      count++
    }
 }
 console.log(count);

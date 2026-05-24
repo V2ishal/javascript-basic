1613739c788029fc0c7c0cf74e15a1b16823fd9a
V2ishal <vishalmhaske928@gmail.com>
2026-05-24 13:24:21 +0530

    string reverse using method solve

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 // string is plaindrome or not
-let str="madam";
+// let str="madam";
 
-let palindrome="true";
+// let palindrome="true";
 
-for(let i=0; i<str.length/2; i++){
-   if(str[i]!==str[str.length-1-i]){
-      palindrome="false";
-   }
-}
-console.log(palindrome);
+// for(let i=0; i<str.length/2; i++){
+//    if(str[i]!==str[str.length-1-i]){
+//       palindrome="false";
+//    }
+// }
+// console.log(palindrome);
+
+//q2 string reverse using method
+let str = "vishal";
+
+let reversed = str.split("").reverse().join("");
+
+console.log(reversed);
 
 
 

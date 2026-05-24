749dadcb3e76fdfa8434a00769aa24eb941a4b59
V2ishal <vishalmhaske928@gmail.com>
2026-05-24 15:20:57 +0530

    update

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
-//q3 count vowels in string using for of loopes
-let str="vishal";
-let count=0;
 
-for(ch of str){
+//Q2 string is palindrome or not?
+let str="madam";
+let palindrome="string is palindrome";
 
-   if(ch==="a"||ch==="e"||ch==="i"||ch==="o"||ch==="u"){
-      count++
+for(let i=0; i<str.length/2; i++){
+  
+   if(str[i]!==str[str.length-1-i]){
+      palindrome="string is not palindrome";
+      break;
    }
 }
-console.log(count);
+console.log(palindrome);
+//q3 count vowels in string using for of loopes
+// let str="vishal";
+// let count=0;
+
+// for(ch of str){
+
+//    if(ch==="a"||ch==="e"||ch==="i"||ch==="o"||ch==="u"){
+//       count++
+//    }
+// }
+// console.log(count);

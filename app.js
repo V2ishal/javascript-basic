2eb2351b8669dbeb83ebc9d1d3980a3436129f97
V2ishal <vishalmhaske928@gmail.com>
2026-05-24 14:01:08 +0530

    string reverse using method slove

--- app.js
+++ app.js
@@ -1,21 +1,10 @@
-// string is plaindrome or not
-// let str="madam";
 
-// let palindrome="true";
+// //q2 string reverse using method
+ let str = "vishal";
 
-// for(let i=0; i<str.length/2; i++){
-//    if(str[i]!==str[str.length-1-i]){
-//       palindrome="false";
-//    }
-// }
-// console.log(palindrome);
+ let reversed = str.split("").reverse().join("");
 
-//q2 string reverse using method
-let str = "vishal";
-
-let reversed = str.split("").reverse().join("");
-
-console.log(reversed);
+ console.log(reversed);
 
 
 

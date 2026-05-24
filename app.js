3a527ccb36478a48e68c01ae1a664851c11067c2
V2ishal <vishalmhaske928@gmail.com>
2026-05-24 15:52:29 +0530

    update

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
-// vowels count using normal for loopes
-let str="rahul";
-let count=0;
 
-for(let i=0; i<str.length; i++){
-   str[i];
-   if(str[i]==="a"||str[i]==="e"||str[i]==="i"||str[i]==="o"||str[i]==="u"){
-      count++
-   }
+//Q2 palindrome or not using methode
+let str="madam";
+let reversed=str.split("").reverse().join("");
+if(str===reversed){
+   console.log("palindrome");
 }
-console.log(count);
+else{
+   console.log("not palindrome");
+}
+// vowels count using normal for loopes
+// let str="rahul";
+// let count=0;
+
+// for(let i=0; i<str.length; i++){
+//    str[i];
+//    if(str[i]==="a"||str[i]==="e"||str[i]==="i"||str[i]==="o"||str[i]==="u"){
+//       count++
+//    }
+// }
+// console.log(count);

410ff615fd2c3c28e17a09ebb1adac32eb96f4c4
vishal <153581112+V2ishal@users.noreply.github.com>
2026-05-25 10:02:19 +0530

    code under missleance code delete

--- app.js
+++ app.js
@@ -11,14 +11,3 @@ for(let i=0; i<str.length/2; i++){
    }
 }
 console.log(palindrome);
-//q3 count vowels in string using for of loopes
-// let str="vishal";
-// let count=0;
-
-// for(ch of str){
-
-//    if(ch==="a"||ch==="e"||ch==="i"||ch==="o"||ch==="u"){
-//       count++
-//    }
-// }
-// console.log(count);

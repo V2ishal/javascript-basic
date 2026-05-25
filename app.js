f05377a1691dbd75c0f40a4c26b0b15303deee4f
V2ishal <vishalmhaske928@gmail.com>
2026-05-25 15:14:04 +0530

    update

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
+//Q4 vovels count using for of loopes
+let str="vishal";
+let count=0;
+for(let ch of str){
+
+   if(ch==="a"|| ch==="e"||ch==="i"||ch==="o"||ch==="u"){
+      count++;
+   }
+}
+console.log(count);
 
 
 

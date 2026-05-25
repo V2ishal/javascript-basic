a9c5c3cfcee3e2826964a6d94a7dec3cde6cc8ba
V2ishal <vishalmhaske928@gmail.com>
2026-05-25 13:35:37 +0530

    extra code delete

--- app.js
+++ app.js
@@ -8,14 +8,3 @@ if(str===reversed){
 else{
    console.log("not palindrome");
 }
-// vowels count using normal for loopes
-// let str="rahul";
-// let count=0;
-
-// for(let i=0; i<str.length; i++){
-//    str[i];
-//    if(str[i]==="a"||str[i]==="e"||str[i]==="i"||str[i]==="o"||str[i]==="u"){
-//       count++
-//    }
-// }
-// console.log(count);

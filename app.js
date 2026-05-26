128c72218fd6a79ddba9d9351b2fe4424d36e399
V2ishal <vishalmhaske928@gmail.com>
2026-05-26 15:42:05 +0530

    update

--- app.js
+++ app.js
@@ -1,20 +1,13 @@
-//q5 duplicate remove not using method
-let str="proggrram";
-let result="";
+//Q6 vowels count using method
+let str="vishal";
+let count=0;
+
 for(let i=0; i<str.length; i++){
-   let ch=str[i];
-   let found="false";
+    let ch=str[i].toLowerCase();
 
-   for(let j=0; j<result.length; j++){
-      if(result[j]===ch){
-         found="true";
-         break;
-      }
-   }
-   if(found==="false"){
-   result+=ch;
+if("aeiou".includes(ch)){
+    count++;
 }
+
 }
-console.log(result);
-
-
+console.log(count);

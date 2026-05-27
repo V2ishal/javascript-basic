d5c0607f3aa63f3c31ada669a11d8ccd21c6b81b
V2ishal <vishalmhaske928@gmail.com>
2026-05-27 14:36:41 +0530

    app.js under string duplcate question solve

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
+// string duplicate remove 
+let str ="abbcdde";
+let result="";
 
+for(let i=0; i<str.length; i++){
+let duplicate="false";
+
+for(let j=0; j<result.length; j++){
+    if(str[i]==result[j]){
+    duplicate="true";
+    break;
+    }
+}
+if(duplicate=="false"){
+    result+=str[i];
+}
+}
+console.log(result); 

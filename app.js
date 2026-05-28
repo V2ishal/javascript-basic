8b20aa318d78ad8f38d51b6233bb6d90ee38e5ba
V2ishal <vishalmhaske928@gmail.com>
2026-05-28 15:04:32 +0530

    app.js empty

--- app.js
+++ app.js
@@ -1,10 +1,1 @@
 
-//Q2 palindrome or not using methode
-let str="madam";
-let reversed=str.split("").reverse().join("");
-if(str===reversed){
-   console.log("palindrome");
-}
-else{
-   console.log("not palindrome");
-}

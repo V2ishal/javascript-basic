b4a00a05a4a888fee8e0b318c969d6f1113657b1
V2ishal <vishalmhaske928@gmail.com>
2026-05-28 15:19:44 +0530

    string is palindrome or notusing method

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-
+//q2 string is palindrome or not using method
+let str="madam";
+let reversed=str.split("").reverse().join("");
+if(reversed===str){
+    console.log("paindrome");
+}
+else{
+    console.log("not a palindrome");
+}

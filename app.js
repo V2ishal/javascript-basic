b732c42480db7e4cc44740990489850b55e66836
V2ishal <vishalmhaske928@gmail.com>
2026-05-30 09:51:39 +0530

    app.js branch e empty

--- app.js
+++ app.js
@@ -1,10 +1,1 @@
-//q4 vowels count using normal for of loopes
-let student="nayan";
-let count=0;
-for(let i=0; i<student.length-1; i++){
-    let ch=student[i];
-    if(ch==="a"||ch==="e"||ch==="i"||ch==="o"||ch==="u"){
-        count++;
-    }
-}
-console.log(count);
+

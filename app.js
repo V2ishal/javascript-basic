4f6c21b0638cb7420c5c217a3fa2d363e8e25c34
V2ishal <vishalmhaske928@gmail.com>
2026-05-30 15:20:59 +0530

    string duplicate remove solve

---
+++ app.js
@@ -0,0 +1,17 @@
+//q 4 string under duplicate remove
+let str="abbcdde";
+let result="";
+
+for(let i=0; i<str.length; i++){
+let duplicate=false;
+    for(let j=0; j<result.length; j++){
+        if(str[i]==result[j]){
+            duplicate=true;
+            break;
+        }
+    }
+    if(duplicate==false){
+        result+=str[i];
+    }
+}
+console.log(result);

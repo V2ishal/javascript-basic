5ba12d677be3c7fda3d0701f1a3e345882370de9
V2ishal <vishalmhaske928@gmail.com>
2026-05-30 13:56:49 +0530

    vowels count using method solve

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
-//vowels count using methode
+//vowels count using method
+let student="nayan";
+let vowels="aeiouAEIOU";
+let count=0;
+
+for(let i=0; i<student.length; i++){
+    let ch=student[i];
+
+    if(vowels.includes(ch)){
+        count++;
+    }
+}
+console.log(count);
 

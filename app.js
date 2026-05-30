8880c27648b322ec1ea800e0bdad95ad6c9e6908
V2ishal <vishalmhaske928@gmail.com>
2026-05-30 12:31:51 +0530

    app .js branch g empty

--- app.js
+++ app.js
@@ -1,13 +1,2 @@
-//Q6 vowels count using method
-let str="vishal";
-let count=0;
+//vowels count using methode
 
-for(let i=0; i<str.length; i++){
-    let ch=str[i].toLowerCase();
-
-if("aeiou".includes(ch)){
-    count++;
-}
-
-}
-console.log(count);

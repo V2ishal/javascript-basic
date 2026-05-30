710f3eb84bca8163515e8ad6494209ad0eccd015
V2ishal <vishalmhaske928@gmail.com>
2026-05-30 10:09:44 +0530

    vovels count using normal for loopes solve

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
+//q3 vovels count using normal for of loopes
+let student="nayan";
+let count=0;
 
+for(let i=0; i<student.length; i++){
+    let ch=student[i];
+
+    if(ch==="a"||ch==="e"||ch==="i"||ch==="o"||ch==="u"){
+        count++
+    }
+}
+console.log(count);

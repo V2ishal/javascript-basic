b827d989b47fdc2b81b026932a0242047ac7b255
V2ishal <vishalmhaske928@gmail.com>
2026-05-31 15:01:03 +0530

    duplicate remove using method solve

--- app.js
+++ app.js
@@ -1,17 +1,11 @@
-//q 4 string under duplicate remove
+//Q 4 duplicate remove using include method
 let str="abbcdde";
 let result="";
 
-for(let i=0; i<str.length; i++){
-let duplicate=false;
-    for(let j=0; j<result.length; j++){
-        if(str[i]==result[j]){
-            duplicate=true;
-            break;
-        }
-    }
-    if(duplicate==false){
-        result+=str[i];
+for(let ch of str){
+
+    if(!result.includes(ch)){
+        result+=ch;
     }
 }
 console.log(result);

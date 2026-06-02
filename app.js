0032b1b5515715a826a0d484b0d71bac1bf2fc81
V2ishal <vishalmhaske928@gmail.com>
2026-06-02 14:30:06 +0530

    app .js branch l update

--- app.js
+++ app.js
@@ -1,12 +1,1 @@
-// first non repeating char in string using method
-let str="swiss";
-let result="";
-
-for(let i=0; i<str.length; i++){
-if(str.indexOf(str[i])===str.lastIndexOf(str[i])){
-    result+=str[i];
-    break;
-}
-}
-console.log(result);
 

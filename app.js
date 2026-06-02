0e792dbf25cb0f243725e7f06790aa63540118d2
V2ishal <vishalmhaske928@gmail.com>
2026-06-02 15:11:23 +0530

    string compression solve not using method

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
+//Q 6 String compression (bonus 🔥)Same characters ko count karke short form me likhte hain
+let str="aaabbc";
+let result="";
+let count=1;
+
+for(let i=0; i<str.length; i++){
+
+    if(str[i]===str[i+1]){
+       count++;
+    }
+    else{
+        result+=str[i]+count;
+        count=1;
+    }
+}
+console.log(result);
 

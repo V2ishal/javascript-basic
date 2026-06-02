2542f4cf2f58d5a6be8ff7d54b94d710c3cbdb16
V2ishal <vishalmhaske928@gmail.com>
2026-06-02 11:16:32 +0530

    update

--- app.js
+++ app.js
@@ -1,19 +1,1 @@
-//Q5 first non repeating char
-let str="swiss";
-let result="";
 
-for(let i=0; i<str.length; i++){
-    let count=0;
-
-    for(j=1; j<str.length; j++){
-
-        if(str[i]==str[j]){
-            count++;
-        }
-    }
-    if(count==1){
-        result+=str[i];
-        break;
-    }
-}
-console.log(result);

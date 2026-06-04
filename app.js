ec9874985c650c18aaff4658e06aba18e4d4eedc
V2ishal <vishalmhaske928@gmail.com>
2026-06-04 13:13:08 +0530

    string compreston using method solve

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ for(let i=0; i<str.length; i++){
         i++;
     }
         result+=str[i]+count;
-     
+        //count++ is line par hum count++ is liye use nhi h rha because humne count variable ko for loopes ke under declare kiya hai
 }
 console.log(result);
 

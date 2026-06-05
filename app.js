da0d9c015e5724944151c163751e3031c4c04f6b
V2ishal <vishalmhaske928@gmail.com>
2026-06-05 14:49:43 +0530

    branch d file app.js empty

--- app.js
+++ app.js
@@ -1,11 +1,1 @@
-//Q.2 string is palindrome or not using method
-let str="nayan";
-let reversed=str.split("").reverse().join("");
-
-if(reversed===str){
-    console.log("string is palindrome");
-}
-else{
-    console.log("string is not palindrome");
-}
 

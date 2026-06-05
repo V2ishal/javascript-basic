7b08a83fc8e45466bb53c372695969c76f2ad246
V2ishal <vishalmhaske928@gmail.com>
2026-06-05 14:03:54 +0530

    string is palindrome or not solve

---
+++ app.js
@@ -0,0 +1,13 @@
+//q2 string is palindrome or not 
+let str="nayan";
+let palindrome="string is palindrome";
+
+for(let i=0; i<str.length/2; i++){
+
+    if(str[i]!==str[str.length-1-i]){
+        palindrome="string is not palindrome";
+        break;
+    }
+}
+console.log(palindrome);
+

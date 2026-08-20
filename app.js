6df2f7f5266ecc958d8ff4b4dcb67429a96feb96
V2ishal <vishalmhaske928@gmail.com>
2026-08-20 15:43:53 +0530

    first non repeating char solved

---
+++ app.js
@@ -0,0 +1,21 @@
+// first non repeating char solved not using method
+let str = "swiss";
+let result = "";
+
+for(let i = 0; i < str.length; i++){
+    let count = 0;
+
+    for(let j = 0; j < str.length; j++){
+
+        if(str[i] == str[j]){
+            count++;
+        }
+    }
+
+    if(count == 1){
+        result += str[i];
+        break;
+    }
+}
+
+console.log(result);

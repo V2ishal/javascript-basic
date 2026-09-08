2bc6b3eb722bbadb38e93710f105ff3f880af2f2
V2ishal <vishalmhaske928@gmail.com>
2026-09-08 16:47:41 +0530

    first non repeating char using for of loopes and include method

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 // first non repeating char in string using method
 let str="swiss";
 let result="";
-
-for(let i=0; i<str.length; i++){
-if(str.indexOf(str[i])===str.lastIndexOf(str[i])){
-    result+=str[i];
-    break;
-}
+for(let ch of str){
+    if(str.indexOf(ch)===str.lastIndexOf(ch)){
+        result+=ch;
+        break;
+    }
 }
 console.log(result);
 
